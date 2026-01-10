cb3742bda19f85699d03d8ec880f4a8bc76f23f2
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2026-01-10 17:08:06 +0100

    Implemented kafka consumer for message sending

---
+++ notificationService/src/main/java/com/oleksandr/kafka/KafkaConsumer.java
@@ -0,0 +1,26 @@
+package com.oleksandr.kafka;
+
+import com.oleksandr.common.notification.NotificationRequest;
+import com.oleksandr.service.MailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final MailService mailService;
+
+    @KafkaListener(topics = "mailNotifications", groupId = "mail_consumer")
+    public void listen(NotificationRequest message){
+        log.debug("Received mail request: Mail type {}, mail address: {} ",
+                message.type(), message.userForMailDTO().mailAddress());
+
+        mailService.sendEmail(message);
+    }
+
+
+}
