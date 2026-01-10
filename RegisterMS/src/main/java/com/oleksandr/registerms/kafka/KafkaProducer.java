6cac386aeda6ee6ea1de3daf80b46eab6a5066d6
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2026-01-10 17:35:35 +0100

    implemented basic kafka producer class to Monolith and RegisterMS

---
+++ RegisterMS/src/main/java/com/oleksandr/registerms/kafka/KafkaProducer.java
@@ -0,0 +1,16 @@
+package com.oleksandr.registerms.kafka;
+
+import com.oleksandr.common.notification.NotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private final KafkaTemplate<String, NotificationRequest> kafkaTemplate;
+
+    public void sendMessage(NotificationRequest message){
+        kafkaTemplate.send("mailNotifications", message.userForMailDTO().mailAddress(), message);
+    }
+}
